8b1bd4ab0cfe5e4c4a9c5f3a5f0a111e8080c034
Debbie Elliott <rigbydebbie@icloud.com>
2022-02-08 14:41:44 +0200

    updated for norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 14:37:29 by deelliot          #+#    #+#             */
-/*   Updated: 2022/01/28 13:31:01 by deelliot         ###   ########.fr       */
+/*   Updated: 2022/01/31 17:52:57 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 4096
+# define BUFF_SIZE 8
 # define MAX_FD 4096
 
 # include "libft/libft.h"
